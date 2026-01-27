480bb627a8e9234c23a7a36728cfe221097460ec
Galih Aditya <ghalih404@gmail.com>
2026-01-27 20:20:15 +0700

    redeploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: 'class', // Enable class-based dark mode
   theme: {
